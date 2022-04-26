fb40fc6745237ba9688cc113b42281042c245b01
A-Saadat <ali.saadat.4950@gmail.com>
2022-04-26 17:49:16 +0430

    update some style and add levels to game

--- Static/main.js
+++ Static/main.js
@@ -1,27 +1,44 @@
-let strat_btn = document.querySelector('#start');
+let strat_btn = document.querySelector('.start');
 let seconds = document.querySelector('.sec');
 let minutes = document.querySelector('.min');
+let next = document.querySelector('#next');
+let pre = document.querySelector('#pre2');
+let cont2 = document.querySelector('.container2');
+let body = document.querySelector('body')
+
 let i = 0, j = 0;
 
 function Interval(index, value, num, limit){
-    setInterval(() => {
+    let t = setInterval(() => {
         index++;
         value.innerHTML = index;
 
-        if(index > limit){
+        if(index >= limit){
             index = -1;
+        }
+
+        if(j >= 4){
+            clearInterval(t);
         }
 
     }, num);
 }
 
 let strat_game = strat_btn.addEventListener('click', () => {
-    Interval(i, seconds, 1000, 10)
-    Interval(j, minutes, (1000 * 59), 4)
-
-    
+    Interval(i, seconds, 1000, 59)
+    Interval(j, minutes, (1000 * 61), 4)
 })  
 
+next.addEventListener('click', () => {
+    cont2.classList.toggle('open');
+    body.classList.toggle('change_bg')
+})
 
+pre.addEventListener('click', () => {
+    cont2.classList.remove('open');
+    cont2.classList.add('close');
+    body.classList.remove('change_bg')
+    body.classList.add('pre_bg')
+})
 
 
